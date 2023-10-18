d55184638e0db557888282d17c855edba346cdfc
Raj Raorane <41839716+Raj-RR1@users.noreply.github.com>
2023-10-18 22:04:55 +0530

    feature-input name and number of arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use core::num;
 use std::io;
 
 struct MyObject {
@@ -9,11 +10,20 @@ struct MyObject {
 }
 fn main() {
     //let mut objects = Vec::new();
-    println!("Enter the number of arguments the extrinsic has");
     let mut num_arguments = String::new();
+    println!("Enter the number of arguments the extrinsic has"); 
     io::stdin().read_line(&mut num_arguments).expect("Failed to read the number of arguments");
     let num_arguments = num_arguments.trim().parse::<usize>().expect("Not able to parse num_arguments");
+    //println!("{}",num_arguments);
 
     let mut argument_names = Vec::new();
+    for i in 0..num_arguments{
+      let mut name = String::new();
+      println!("Enter the name of argument {}:",i+1); 
+      io::stdin().read_line(&mut name).expect("Failed to read argument name");
+      let name=name.trim().to_string();
+      //println!("{}",name);
+      argument_names.push(name);
+    }
 
 }
